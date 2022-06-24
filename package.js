40a92d18fa1586cdb005d3a6fe00b419172ca585
4fox4 <hanafi.o95@gmail.com>
2022-06-24 10:11:24 +0200

    Update packages (Fix migration accounts-base)

--- package.js
+++ package.js
@@ -1,23 +1,24 @@
 Package.describe({
-  name: 'ndev:mfa',
-  version: '0.1.2',
-  summary: 'Multi Factor Authentication and Passwordless (supporting U2F, TOTP, and OTP)',
-  git: 'https://github.com/TheRealNate/meteor-mfa',
-  documentation: 'README.md'
+  name: "4fox4:mfa",
+  version: "0.1.3",
+  summary:
+    "Multi Factor Authentication and Passwordless (supporting U2F, TOTP, and OTP)",
+  git: "https://github.com/4fox4/meteor-mfa",
+  documentation: "README.md",
 });
 
 Npm.depends({
-  "@webauthn/server":"0.1.3",
-  "@webauthn/client":"0.1.3",
-  "otplib":"12.0.0"
+  "@webauthn/server": "0.1.3",
+  "@webauthn/client": "0.1.3",
+  otplib: "12.0.0",
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.8');
-  api.use('ecmascript');
-  api.use('accounts-base');
-  api.use('random');
-  api.use('check');
-  api.mainModule('mfa-client.js', "client");
-  api.mainModule('mfa-server.js', "server");
+Package.onUse(function (api) {
+  api.versionsFrom("2.5.1");
+  api.use("ecmascript");
+  api.use("accounts-base");
+  api.use("random");
+  api.use("check");
+  api.mainModule("mfa-client.js", "client");
+  api.mainModule("mfa-server.js", "server");
 });
